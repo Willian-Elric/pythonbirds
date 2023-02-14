239bfebcee53a49617c1a3bcea754b5c12f3f0cd
Willian-Elric <willianaraujo125@gmail.com>
2023-02-14 01:20:56 -0300

    Concluído todos os testes com sucesso

--- testes/testes_atores.py
+++ testes/testes_atores.py
@@ -112,8 +112,8 @@ class AtorTestes(TestCase):
 
     def assert_colisao_atores_ativos(self, ator, ator2, intervalo=1):
         """
-        Se certifica que há colisão entre atores ativos
-        Atenção: Esse não é método de teste porque nao se inicia com prefixo "text".
+        Se certifica de que há colisão entre atores ativos
+        Atenção: Esse não é método de teste porque nao se inicia com prefixo "test".
         Ele serve apenas para encapsular toda lógica de teste de colisão entre dois atores ativos
         """
         # Conferindo status dos dois atores antes da colisão
@@ -129,7 +129,7 @@ class AtorTestes(TestCase):
         Se certifica que não colisão entre dois atores
         Atenção: Esse não é método de teste porque nao se inicia com prefixo "text".
         Ele apenas encapsula a lógica de não colisão entre dois atores.
-        So seja, eles deve manter seus respectivos status mesmo depois da chamada do metodo colidir
+        So seja, eles devem manter seus respectivos status mesmo depois da chamada do metodo colidir
         """
         # Armazenando status antes da colisão
         status_inicial_ator = ator.status
@@ -264,8 +264,6 @@ class PassaroAmareloTests(PassaroBaseTests):
         """
         passaro_amarelo = PassaroAmarelo(1, 1)
         passaro_amarelo.lancar(90, 2)  # passaro lancado a 90 graus no tempo 2 segundos
-
-
 
         # subindo
 
